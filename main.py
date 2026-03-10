1c2bdf67a72cf23757e033b7f56c6fcca85f2b83
João Souza <itsjvsouza@gmail.com>
2026-03-10 00:33:18 -0300

    feat: adiciona leitura de arquivo

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from datetime import datetime
+from datetime import date
 
 while True:
 
-    inicio = int(input('\n1 - Adicionar gasto \n2 - Consultar gastos \n\nDigite o número correspondente à ação desejada: '))
+    inicio = int(input('\n1 - Adicionar gasto \n2 - Consultar gastos \n3 - Encerrar \n\nDigite o número correspondente à ação desejada: '))
 
     if inicio == 1:
 
         while True:
-            d = int(input(('\n1 - Utilizar data de hoje \n2 - Digitar data \n\nOpção: ')))
+            d = int(input(('\n1 - Utilizar data de hoje \n2 - Digitar data \n3 - Voltar \n\nOpção: ')))
 
             if d == 1:
-                data = datetime.today()
+                data = date.today()
                 data_usuario = data.strftime('%d/%m/%Y')
                 
             elif d == 2:
                 data_usuario = str(input('Digite a data (DD/MM/YYYY): '))
                 data_obj = datetime.strptime(data_usuario, '%d/%m/%Y')
                 data = data_obj.strftime('%Y-%m-%d')
-                
+
+            elif d == 3:
+                break
+
             else:
                 print('Ação inválida.')
                 continue
@@ -25,10 +29,10 @@ while True:
             descricao = str(input('Descrição: '))
             valor = float(input('Valor: R$'))
             forma = str(input('Forma (Pix ou Crédito): '))
-            confirmacao = int(input(f'{data_usuario}, {descricao}, {valor}, {forma} \n1 - Sim \n2 - Não \n\nOs dados estão corretos? '))
+            confirmacao = int(input(f'\n{data_usuario}, {descricao}, {valor}, {forma} \n\n1 - Sim \n2 - Não \n\nOs dados estão corretos? '))
 
             if confirmacao == 1:
-                linha = (f'{data}, {descricao}, {valor}, {forma}')
+                linha = (f'{data}, {descricao}, R${valor}, {forma}')
 
                 with open('data/gastos.csv', 'a') as arquivo:
                     arquivo.write(linha)
@@ -38,3 +42,15 @@ while True:
             
             else:
                 continue
+    
+    elif inicio == 2:
+        with open('data/gastos.csv', 'r') as arquivo:
+            linhas = arquivo.readlines()
+        
+            print(linhas)
+    
+    elif inicio == 3:
+        break
+
+    else:
+        print('Ação inválida.')
